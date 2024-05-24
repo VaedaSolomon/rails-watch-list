7dd0e61e2c9d7e506566e97085b75f8bd1c7fbe6
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-05-24 11:46:31 -0400

    core completed with some html code from challenge solution

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,9 +8,18 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      @review = Review.new
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-    redirect_to list_path(@bookmark)
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   def bookmark_params
